1049534161b6e628c40341c8a1c898300a2cb5f2
rachita28 <rachitarathore91@gmail.com>
2026-04-30 05:53:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Sabhi origins allow karne ke liye (Deployment testing ke liye best hai)
-app.use(cors()); 
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 
 app.use(morgan("dev"));
 
